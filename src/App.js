6306378c26b90d9b37fa75d848d6b637ee47fa79
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-02-08 20:53:37 +0200

    Added the components and their css.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react'; 
 import Home from './pages/Home';
+import Header from "./components/Header";
+import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
 
 function App() {
   return (
    <div className='App'>
-< Home/>
+    <Router>
+      <Header/>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={< Home/>}></Route>
+    </Routes>
+    <Footer/>
+  </Router>  
    </div>
   );
 }

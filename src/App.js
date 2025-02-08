d5a1621c536bdacfb1960929755d40157cf35d44
Zinhle Nhlapo <zinhle.nhlapo31@gmail.com>
2025-02-08 21:27:55 +0200

    Added the beauty page and its css.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Header from "./components/Header";
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
+import Beauty from './pages/Beauty';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element={< Home/>}></Route>
+        <Route path="/beauty" element={<Beauty/>}></Route>
     </Routes>
     <Footer/>
   </Router>  
